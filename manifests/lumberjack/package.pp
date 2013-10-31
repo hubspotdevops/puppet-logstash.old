552c1cca3766186b0f708b07537d5c700881ab54
Eddie Kay <ekay@hubspot.com>
2013-10-31 16:08:03 +0000

    Peg lumberjack to version 0.0.30-1.

--- manifests/lumberjack/package.pp
+++ manifests/lumberjack/package.pp
@@ -1,6 +1,6 @@
 class logstash::lumberjack::package (
   $package_name    = 'lumberjack',
-  $package_version = 'installed',
+  $package_version = '0.0.30-1',
 ) {
   package { $package_name:
     ensure => $package_version
