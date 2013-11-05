a9e8c4a6d3eb502f6dd2683775a3a80d7d67e391
Eddie Kay <ekay@hubspot.com>
2013-11-05 20:13:46 +0000

    Add ssl cert and key parameters.

--- manifests/lumberjack2.pp
+++ manifests/lumberjack2.pp
@@ -2,6 +2,10 @@ class logstash::lumberjack2 (
   $files,
   $logstash_servers,
   $package_version = 'installed',
+  $ssl_cert_path   = undef,
+  $ssl_cert_source = undef,
+  $ssl_key_path    = undef,
+  $ssl_key_source  = undef,
   $ssl_ca_path     = '/etc/lumberjack/lumberjack.crt',
   $ssl_ca_source   = undef,
   $timeout         = 15,
@@ -11,11 +15,15 @@ class logstash::lumberjack2 (
   }
 
   class { '::logstash::lumberjack::config':
-    servers       => $logstash_servers,
-    timeout       => $timeout,
-    files         => $files,
-    ssl_ca_path   => $ssl_ca_path,
-    ssl_ca_source => $ssl_ca_source,
+    servers         => $logstash_servers,
+    timeout         => $timeout,
+    files           => $files,
+    ssl_cert_path   => $ssl_cert_path,
+    ssl_cert_source => $ssl_cert_source,
+    ssl_key_path    => $ssl_key_path,
+    ssl_key_source  => $ssl_key_source,
+    ssl_ca_path     => $ssl_ca_path,
+    ssl_ca_source   => $ssl_ca_source,
   }
   Class['::logstash::lumberjack::package'] -> Class['::logstash::lumberjack::config']
 }
